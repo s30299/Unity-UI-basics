955d245600b05b1fc51daa508937b28e2a827bf3
Oliwier Marcewicz <154228001+s30299@users.noreply.github.com>
2025-09-27 18:55:42 +0200

    Pause Menu logic added

--- Assets/Scripts/UIManagement2.cs
+++ Assets/Scripts/UIManagement2.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class UIManagement2 : MonoBehaviour
 {
+    public GameObject pauseUI;
     public void OnRestartPress()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void OnGameResumePress()
+    {
+        pauseUI.SetActive(false);
+
+    }
+    public void OnGameExitPress()
+    {
+        Application.Quit();
+    }
+
+    public void OnEnterPausePress()
+    {
+        pauseUI.SetActive(true);
     }
 }
